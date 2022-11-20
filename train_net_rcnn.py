1b8a50388c84dd2644b6feba582cd934ade852e5
Zhiqing Sun <zhiqings@cs.cmu.edu>
2022-11-19 20:44:59 -0500

    Update train_net_rcnn.py

--- train_net_rcnn.py
+++ train_net_rcnn.py
@@ -360,7 +360,7 @@ def setup(args):
     Create configs and perform basic setups.
     """
     cfg = get_cfg()
-    add_troi_config(cfg)
+    add_rcnn_config(cfg)
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.freeze()
